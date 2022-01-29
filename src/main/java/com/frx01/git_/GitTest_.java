ffe60adc6c0f3521d79285885f617e3d7d6eb3bd
xustudyxu <1812903531@qq.com>
2022-01-29 14:33:53 +0800

    first commit

---
+++ src/main/java/com/frx01/git_/GitTest_.java
@@ -0,0 +1,12 @@
+package com.frx01.git_;
+
+/**
+ * @author frx
+ * @version 1.0
+ * @date 2022/1/29  14:29
+ */
+public class GitTest_ {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
