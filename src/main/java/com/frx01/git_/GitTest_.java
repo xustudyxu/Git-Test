df87868688a130934b69ed4b0cd278e5ec69f492
xustudyxu <1812903531@qq.com>
2022-01-29 15:42:10 +0800

    push commit

--- src/main/java/com/frx01/git_/GitTest_.java
+++ src/main/java/com/frx01/git_/GitTest_.java
@@ -13,5 +13,6 @@ public class GitTest_ {
         System.out.println("Hello git!4");
         System.out.println("master test!");
         System.out.println("hot-fix test");
+        System.out.println("push-text");
     }
 }
