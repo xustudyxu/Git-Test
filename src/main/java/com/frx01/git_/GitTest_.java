4757441766e7149c2ae1c001c0dcef9c9c8f6928
xustudyxu <1812903531@qq.com>
2022-01-29 14:40:09 +0800

    third commit

--- src/main/java/com/frx01/git_/GitTest_.java
+++ src/main/java/com/frx01/git_/GitTest_.java
@@ -9,5 +9,6 @@ public class GitTest_ {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git!2");
+        System.out.println("Hello git!3");
     }
 }
